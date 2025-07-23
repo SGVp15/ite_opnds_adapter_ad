249f9626fa1980ed0a8500a57acfbcd4cd8aaf4f
SGVp15 <phosphorusp15@mail.ru>
2025-07-23 20:33:51 +0300

    main

--- test_parser.py
+++ test_parser.py
@@ -1,19 +1,22 @@
+import os
 import re
 from unittest import TestCase
 
+from config import DIR_OUT
+from main import save_users
 from parser import parser_users
 
 
 class Test(TestCase):
     def test_parser_users(self):
         for i in range(1):
-            with open('out2.txt', 'r', encoding='utf-8') as f:
+            with open('./input_test/out2.txt', 'r', encoding='utf-8') as f:
                 s = f.read()
             o = parser_users(s)
             print(o)
 
         for i in range(1):
-            with open('out_user.txt', 'r', encoding='utf-8') as f:
+            with open('./input_test/out_user.txt', 'r', encoding='utf-8') as f:
                 s = f.read()
-            o = parser_users(s)
-            print(o)
+            users = parser_users(s)
+            save_users(users_data=users, csv_filename=str(os.path.join(DIR_OUT, 'out_user.txt')))
